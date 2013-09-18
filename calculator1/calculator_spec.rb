eeed2182fbc1db32a744511eea1edebce1033472
leszek <andrukanisleszek@gmail.com>
2013-09-18 12:57:16 +0200

    Step4

--- calculator1/calculator_spec.rb
+++ calculator1/calculator_spec.rb
@@ -1,4 +1,6 @@
 require 'minitest/autorun'
+# DO NOT USE SOPHISTICATED CODE IN UNITS, BECAUSE YOU'LL HAVE TO WRITE TESTS TO TESTS
+# MOREOVER CHANGES COST A LOT THEN
 
 class TestCalculator < MiniTest::Unit::TestCase
   def setup
@@ -9,30 +11,65 @@ class TestCalculator < MiniTest::Unit::TestCase
     assert_equal 0, @calc.Add("")
   end
 
-  def test_one_string_input
-    assert_equal 2, @calc.Add("2")
-  end
-
-  def test_two_string_input
-    assert_equal 3, @calc.Add("2,1")
-  end
-
-  def test_random_number_of_string_inputs
+  # only commas context 
+  def test_random_number_of_string_inputs_as_commas
     10.times do
-      digits = rand(100).times.map{ rand(10) }
+      digits = (rand(100)+1).times.map{ rand(10) }
 
       assert_equal digits.inject(:+), @calc.Add(digits.join(','))
     end
   end
 
+  # only new lines context
+  def test_random_number_of_string_inputs_as_new_lines
+    10.times do
+      digits = (rand(100)+1).times.map{ rand(10) }
 
+      assert_equal digits.inject(:+), @calc.Add(digits.join('\n'))
+    end
+  end
+
+  # mixed new lines and commas context
+  def test_random_number_of_string_inputs_as_mixed
+    10.times do
+      digits = rand(100).times.map{ rand(10) }
+      range = rand(digits.size-1) + 1
+      input = digits[0..range].join(',') + "," + digits[(range + 1)..-1].join('\n')
+      assert_equal digits.inject(:+), @calc.Add(input)
+    end
+  end
+
+
+  def test_custom_delimiter_support
+    input = '//;;\n1;;2;;3;;4;;5'
+
+    assert_equal 15, @calc.Add(input)
+  end
 end
 
 class Calculator
 
   def self.Add(input)
-    digits = input.split(',')
+    digits = (has_delimiter?(input) ? custom_splitter(input) : default_splitter(input))
     sum = digits.map(&:to_i).inject(:+)
     sum || 0
+  end
+
+  private
+
+  def self.has_delimiter?(input)
+    (input =~ /\/\/.+\\n/) == 0
+  end
+
+  def self.default_splitter(input)
+    input.gsub('\n', ',').split(',')
+  end
+
+  def self.custom_splitter(input)
+    input.split('\n')[1].split(get_delimitter(input))
+  end
+
+  def self.get_delimitter(input)
+    input.scan(/\/\/.+\\n/).first[2..-3]
   end
 end
