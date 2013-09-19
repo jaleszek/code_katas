343c02557a5dce7743028c6a1ad47866520584f9
Leszek <andrukanisleszek@gmail.com>
2013-09-19 10:33:55 +0200

    Step2

--- calculator2/calculator_spec.rb
+++ calculator2/calculator_spec.rb
@@ -10,11 +10,26 @@ class TestCalculator < MiniTest::Unit::TestCase
     assert_equal 0, @calculator.Add('')
   end
 
-  def test_for_various_length_inputs
+  # context for commas only
+  def test_for_various_length_inputs_for_commas
     assert_equal 10, @calculator.Add('10')
     assert_equal 20, @calculator.Add('10,10')
     assert_equal 30, @calculator.Add('10,10,10')
     assert_equal 101, @calculator.Add('1,0,0,0,100')
+  end
+
+  # context for new lines only
+  def test_for_various_length_inputs_for_new_lines
+    assert_equal 30, @calculator.Add('10\n10\n10')
+    assert_equal 30, @calculator.Add('10\n5\n5\n10')
+    assert_equal 30, @calculator.Add('15\n15')
+  end
+
+  # context for new lines and commas mixed
+  def test_for_various_length_inputs_for_mixed_delimiters
+    assert_equal 30, @calculator.Add('10,10\n10')
+    assert_equal 30, @calculator.Add('5\n5\n5,5\n5,5')
+    assert_equal 30, @calculator.Add('5\n5\n5,5,5\n5')
   end
 
 end
