7b124d1679683943bed70a8a030c9dc937d028b1
Leszek <andrukanisleszek@gmail.com>
2013-10-03 13:35:19 +0200

    First step

--- calculator4/calculator_spec.rb
+++ calculator4/calculator_spec.rb
@@ -16,6 +16,19 @@ class TestCalculator < MiniTest::Unit::TestCase
   def test_two_arguments_input
     assert_equal 4, @calculator.Add('2,2')
   end
+
+  def test_various_number_of_arguments
+    inputs = {
+      10 => '5,2,3',
+      20 => '5,5,5,5',
+      30 => '5,5,5,5,5,5',
+      50 => '5,5,10,10,5,5,5,5'
+    }
+    inputs.each do |sum, input|
+      assert_equal sum, input
+    end
+
+  end
 end
 
 
